66f07e98a895408158332188274fe3d5477e3010
Lorenzo Ciriaco <ciriacolorenzo5@gmail.com>
2025-08-16 19:17:58 -0300

    adding class Empleado

--- src/dominio/Empleado.java
+++ src/dominio/Empleado.java
@@ -1,5 +1,43 @@
 package dominio;
 
-public class Empleado {
+import java.time.LocalDate;
 
+public class Empleado extends Persona {
+    // Atributos propios
+    private static int contadorLegajo = 1000;
+    private int legajo;
+    private String puesto;
+
+    // Constructor
+    public Empleado(String dni, String nombre, String apellido, LocalDate fechaNacimiento,
+                    String genero, String direccion, String telefono, String email,
+                    String puesto) {
+        super(dni, nombre, apellido, fechaNacimiento, genero, direccion, telefono, email);
+        this.legajo = contadorLegajo++; // se asigna y luego se incrementa
+        this.puesto = puesto;
+    }
+
+    // Getter solo de lectura para legajo (no setter, porque es autogenerado)
+    public int getLegajo() {
+        return legajo;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    // Método toString
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "legajo=" + legajo +
+                ", puesto='" + puesto + '\'' +
+                ", datosPersona=" + super.toString() +
+                '}';
+    }
 }
+
--- src/dominio/Principal.java
+++ src/dominio/Principal.java
@@ -1,11 +1,21 @@
 package dominio;
 
+import java.time.LocalDate;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		
 		Persona p = new Persona();
 		System.out.println(p.toString());
+		
+		Empleado e1 = new Empleado(
+                "12345678", "Lolo", "Ciriaco", LocalDate.of(1998, 5, 12),
+                "Masculino", "Av. Siempre Viva 123", "123456789", "lolo@mail.com",
+                "Programador"
+        );
+		
+		System.out.println(e1.toString());
 
 	}
 
